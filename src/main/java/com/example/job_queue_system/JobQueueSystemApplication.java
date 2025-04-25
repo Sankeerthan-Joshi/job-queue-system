b5302627aa00e69c4b9e32613e02ccd640b763e1
Sankeerthan-Joshi <sankeerthana.a.j@gmail.com>
2025-04-25 22:30:10 +0530

    Initial Commit

---
+++ src/main/java/com/example/job_queue_system/JobQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_queue_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobQueueSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobQueueSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job_queue_system/JobQueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_queue_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQueueSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
